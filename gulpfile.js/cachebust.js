6b8ae078161e27780f911b5e819a6f3c046ba7f3
Cherry Wang <chryw@catenology.com>
2017-12-17 22:35:48 -0800

    fix ignore folder

--- gulpfile.js/cachebust.js
+++ gulpfile.js/cachebust.js
@@ -3,6 +3,6 @@ const replace = require('gulp-replace');
 
 const timestamp = Math.round(Date.now() / 1000);
 
-gulp.task('cachebust', ['build'], () => gulp.src(['./_site/**/*.html', './_site/**/*.md', './_site/**/*.markdown'])
+gulp.task('cachebust', ['build'], () => gulp.src(['./dist/**/*.html', './dist/**/*.md', './dist/**/*.markdown'])
   .pipe(replace(/@@hash/g, timestamp))
-  .pipe(gulp.dest('./_site')));
+  .pipe(gulp.dest('./dist')));
